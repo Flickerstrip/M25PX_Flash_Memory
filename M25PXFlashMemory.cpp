fd1987671134f79ecba9b09548dede50f2d66ac4
Julian Hartline <julianh2o@gmail.com>
2016-09-23 16:33:01 -0700

    updating for the M25PX which supports erasure by page and the OTP section

--- M25PXFlashMemory.cpp
+++ M25PXFlashMemory.cpp
@@ -1,11 +1,11 @@
-#include "FlashMemory.h"
+#include "M25PXFlashMemory.h"
 
 //Public Methods
-FlashMemory::FlashMemory(byte sck,byte mosi,byte miso,byte cs) : spi(sck,mosi,miso,cs) {
+M25PXFlashMemory::M25PXFlashMemory(byte sck,byte mosi,byte miso,byte cs) : spi(sck,mosi,miso,cs) {
     spi.begin();
 }
 
-byte FlashMemory::readStatus() {
+byte M25PXFlashMemory::readStatus() {
     spi.select();
     spi.transfer(READ_STATUS_REGISTER);
     byte status = spi.transfer(0);
@@ -13,27 +13,27 @@ byte FlashMemory::readStatus() {
     return status;
 }
 
-bool FlashMemory::isBusy() {
+bool M25PXFlashMemory::isBusy() {
     return readStatus() & (1 << 0); //LSB is WIP
 }
 
-bool FlashMemory::isWritable() {
+bool M25PXFlashMemory::isWritable() {
     return readStatus() & (1 << 1); //bit2 is WEL
 }
 
-void FlashMemory::writeByte(uint32_t addr,byte b) {
+void M25PXFlashMemory::programByte(uint32_t addr,byte b) {
     while(isBusy()) delay(0);
 
     enableWrite(); //write is disabled automatically afterwards
 
     spi.select();
-    spi.transfer(PAGE_WRITE);
+    spi.transfer(PAGE_PROGRAM);
     sendAddress(addr);
     spi.transfer(b);
     spi.deselect();
 }
 
-void FlashMemory::writeBytes(uint32_t addr, byte * buf, int len) {
+void M25PXFlashMemory::programBytes(uint32_t addr, byte * buf, int len) {
     int i=0;
     while (i < len) {
         int writeSize = 0x100 - (addr & 0xff);
@@ -49,7 +49,7 @@ void FlashMemory::writeBytes(uint32_t addr, byte * buf, int len) {
 //            Serial.print(" ");
 //        }
 //        Serial.println();
-        writePage(addr,buf,writeSize);
+        programPage(addr,buf,writeSize);
         addr &= 0xffffff00; //start at the beginning of the page
         addr += 0x00000100; //increment page by one
         buf += writeSize;
@@ -58,7 +58,7 @@ void FlashMemory::writeBytes(uint32_t addr, byte * buf, int len) {
 }
 
 
-byte FlashMemory::readByte(uint32_t addr) {
+byte M25PXFlashMemory::readByte(uint32_t addr) {
     while(isBusy()) delay(0);
 
     spi.select();
@@ -70,7 +70,7 @@ byte FlashMemory::readByte(uint32_t addr) {
     return value;
 }
 
-void FlashMemory::readBytes(uint32_t addr, byte * buf, int len) {
+void M25PXFlashMemory::readBytes(uint32_t addr, byte * buf, int len) {
     int i=0;
     while (i < len) {
         int readSize = 0x100 - (addr & 0xff);
@@ -83,39 +83,96 @@ void FlashMemory::readBytes(uint32_t addr, byte * buf, int len) {
     }
 }
 
-void FlashMemory::erasePage(uint32_t addr) {
-    addr = addr & 0xffffff00; //target page
+void M25PXFlashMemory::readOTP(uint32_t addr, byte * buf, int len) {
+    while(isBusy()) delay(0);
+
+    spi.select();
+    spi.transfer(READ_OTP);
+    sendAddress(addr);
+
+    spi.transfer(0); //pass over the dummy byte
+
+    for (int i=0; i<len; i++) {
+        buf[i] = spi.transfer(0);
+    }
+    spi.deselect();
+}
+
+void M25PXFlashMemory::programOTP(uint32_t addr, byte * buf, int len) {
+    while(isBusy()) delay(0);
+
+    enableWrite(); //write is disabled automatically afterwards
+
+    spi.select();
+    spi.transfer(PROGRAM_OTP);
+    sendAddress(addr);
+
+    for (int i=0; i<len; i++) {
+        spi.transfer(buf[i]);
+    }
+    spi.deselect();
+}
+
+void M25PXFlashMemory::freezeOTP() {
+    while(isBusy()) delay(0);
+
+    enableWrite(); //write is disabled automatically afterwards
+
+    spi.select();
+    spi.transfer(PROGRAM_OTP);
+
+    sendAddress(64); //the write lock
+
+    spi.transfer(0b000); //write 0s to prevent writing
+
+    spi.deselect();
+}
+
+void M25PXFlashMemory::eraseSubsector(uint32_t addr) {
+    addr = addr & 0xfffff000; //target subsector
 
     while(isBusy()) delay(0);
 
     enableWrite(); //write is disabled automatically afterwards
 
     spi.select();
-    spi.transfer(PAGE_ERASE);
+    spi.transfer(SUBSECTOR_ERASE);
     sendAddress(addr);
     spi.deselect();
 }
 
+void M25PXFlashMemory::bulkErase() {
+    while(isBusy()) delay(0);
+
+    enableWrite(); //write is disabled automatically afterwards
+
+    spi.select();
+    spi.transfer(BULK_ERASE);
+    spi.deselect();
+
+    while(isBusy()) delay(0); //BULK_ERASE can take awhile, lets just wait until it completes
+}
+
 //Private Methods
-void FlashMemory::enableWrite() {
+void M25PXFlashMemory::enableWrite() {
     spi.select();
     spi.transfer(WRITE_ENABLE);
     spi.deselect();
 }
 
-void FlashMemory::disableWrite() {
+void M25PXFlashMemory::disableWrite() {
     spi.select();
     spi.transfer(WRITE_DISABLE);
     spi.deselect();
 }
 
-void FlashMemory::sendAddress(uint32_t addr) {
+void M25PXFlashMemory::sendAddress(uint32_t addr) {
     spi.transfer((addr & 0xff0000) >> 16); //addr 0
     spi.transfer((addr & 0xff00) >> 8); //addr 1
     spi.transfer((addr & 0xff)); //addr 2
 }
 
-void FlashMemory::writePage(uint32_t addr, byte * buf, int len) {
+void M25PXFlashMemory::programPage(uint32_t addr, byte * buf, int len) {
     while(isBusy()) delay(0);
 
     enableWrite(); //write is disabled automatically afterwards
@@ -132,7 +189,7 @@ void FlashMemory::writePage(uint32_t addr, byte * buf, int len) {
 //    Serial.println("]");
 
     spi.select();
-    spi.transfer(PAGE_WRITE);
+    spi.transfer(PAGE_PROGRAM);
     sendAddress(addr);
     for (int i=0; i<len; i++) {
         spi.transfer(buf[i]);
@@ -140,7 +197,7 @@ void FlashMemory::writePage(uint32_t addr, byte * buf, int len) {
     spi.deselect();
 }
 
-void FlashMemory::readPage(uint32_t addr, byte * buf, int len) {
+void M25PXFlashMemory::readPage(uint32_t addr, byte * buf, int len) {
     while(isBusy()) delay(0);
 
     spi.select();
--- M25PXFlashMemory.h
+++ M25PXFlashMemory.h
@@ -1,5 +1,5 @@
-#ifndef FlashMemory_h
-#define FlashMemory_h
+#ifndef M25PXFlashMemory_h
+#define M25PXFlashMemory_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
@@ -13,37 +13,46 @@
 #define WRITE_ENABLE                    0x06
 #define WRITE_DISABLE                   0x04
 #define READ_IDENTIFICATION             0x9f
+#define READ_IDENTIFICATION2            0x9e
 #define READ_STATUS_REGISTER            0x05
 #define WRITE_STATUS_REGISTER           0x01
 #define WRITE_LOCK_REGISTER             0xe5
 #define READ_LOCK_REGISTER              0xe8
 #define READ_DATA_BYTES                 0x03
 #define READ_DATA_BYTES_FAST            0x0b
-#define PAGE_WRITE                      0x0a
+#define DUAL_OUTPUT_FAST_READ           0x3b
+#define READ_OTP                        0x4b
+#define PROGRAM_OTP                     0x42
 #define PAGE_PROGRAM                    0x02
-#define PAGE_ERASE                      0xdb
+#define DUAL_INPUT_FAST_PROGRAM         0xa2
 #define SUBSECTOR_ERASE                 0x20
 #define SECTOR_ERASE                    0xd8
 #define BULK_ERASE                      0xc7
 #define DEEP_POWER_DOWN                 0xb9
 #define RELEASE_FROM_DEEP_POWER_DOWN    0xab
 
-class FlashMemory {
+class M25PXFlashMemory {
 public:
-    FlashMemory(byte, byte, byte, byte);
+    M25PXFlashMemory(byte, byte, byte, byte);
     byte readStatus();
     bool isBusy();
     bool isWritable();
-    void writeByte(uint32_t addr,byte b);
-    void writeBytes(uint32_t addr, byte * buf, int len);
+    void programByte(uint32_t addr,byte b);
+    void programBytes(uint32_t addr, byte * buf, int len);
     byte readByte(uint32_t addr);
     void readBytes(uint32_t addr, byte * buf, int len);
-    void erasePage(uint32_t addr);
+
+    void readOTP(uint32_t addr, byte * buf, int len);
+    void programOTP(uint32_t addr, byte * buf, int len);
+    void freezeOTP();
+
+    void eraseSubsector(uint32_t addr);
+    void bulkErase();
 
     void enableWrite();
     void disableWrite();
     void sendAddress(uint32_t addr);
-    void writePage(uint32_t addr, byte * buf, int len);
+    void programPage(uint32_t addr, byte * buf, int len);
     void readPage(uint32_t addr, byte * buf, int len);
 
 private:
